cb759ebcafe7f6d278566c42b100357ecc5be035
lans <nazarsalomon123@gmail.com>
2025-03-14 00:03:47 -0600

    De nuevo a railwy

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Ejecutar migraciones de Laravel (si deseas que se ejecuten en cada deploy)
-php artisan migrate --force
-
-# Iniciar Supervisor, que levantará Nginx y PHP‑FPM
-exec supervisord -n
